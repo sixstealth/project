1d9a1bb63913f8435bd86c5687e7ecf63b0422aa
sixstealth <shamandarkk@gmail.com>
2025-02-02 18:43:35 +0100

    added authentication

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -6,28 +6,44 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['SESSION_COOKIE_NAME'] = 'session'
 
 db = SQLAlchemy(app)
 
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-# # class User(db>model, UserMixin):
-# #     id = db.Column(db.String(150), unique = True, nullable = False)
-# #     password = db.Column(db.String(150), nullable = False)
+login_manager = LoginManager()
+login_manager.login_view = 'login'
 
-# @login_manager.user.loader
-# def load_user(user_id):
-#     return User.query.get(int(user_id))
 
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-# class User(db>model, UserMixin):
-#     id = db.Column(db.String(150), unique = True, nullable = False)
-#     password = db.Column(db.String(150), nullable = False)
 
-# @login_manager.user.loader
-# def load_user(user_id):
-#     return User.query.get(int(user_id))
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(150), unique = True, nullable = False)
+    password = db.Column(db.String(150), nullable = False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('admin'))
+        else:
+            flash('Wrong Credentials')
+
+    return render_template('login.html')
+
+
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -43,6 +59,7 @@ def index():
     return render_template("index.html")
 
 @app.route('/addform')
+@login_required
 def addform():
     return render_template("addform.html")
 
@@ -51,8 +68,16 @@ def about():
     return render_template("about.html")
 
 @app.route('/admin')
+@login_required
 def admin():
+    print(current_user.is_authenticated)
     return render_template("admin.html")
+
+@app.route('/logout', methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 with app.app_context():
     db.create_all()
