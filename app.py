d1199e12dd158a31da5d279bb1625a2cc17bd2e3
sixstealth <shamandarkk@gmail.com>
2025-02-02 15:30:31 +0100

    style added, login being created

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+class User(db>model, UserMixin):
+    id = db.Column(db.String(150), unique = True, nullable = False)
+    password = db.Column(db.String(150), nullable = False)
+
+@login_manager.user.loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String, nullable = False)
     price = db.Column(db.Float, nullable = False)
     isActive = db.Column(db.Boolean, default = True)
     text = db.Column(db.Text, nullable = False)
+
+
 
 @app.route('/')
 def index():
